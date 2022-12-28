13f8d5559aec1ed58b46bb4dae8dfd968462428d
MinnieMinwoo <nhs075241@gmail.com>
2022-12-28 15:59:40 +0900

    add image logic

--- notifications-page-main/script/import.js
+++ notifications-page-main/script/import.js
@@ -1,41 +1,49 @@
 let dataArr = [
     {   
         user: "Mark Webber",
+        userImage: "assets/images/avatar-mark-webber.webp",
         type: "react",
         post: "My first tournament today!",
         time: "1m ago",
     },
     {
         user: "Angela Gray",
+        userImage: "assets/images/avatar-angela-gray.webp",
         type: "follow",
         data: "Angela Gray followed you",
         time: "5m ago",
     },
     {
         user: "Jacob Thompson",
+        userImage: "assets/images/avatar-jacob-thompson.webp",
         type: "join",
         group: "Chess Club",
         time: "1 day ago",
     },
     {
         user: "Rizky Hasanuddin",
+        userImage: "assets/images/avatar-rizky-hasanuddin.webp",
         type: "message",
         message: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
         time: "5 days ago",
     },
     {
         user: "Kimberly Smith",
+        userImage: "assets/images/avatar-kimberly-smith.webp",
         type: "commentPicture",
+        replyPicture: "assets/images/image-chess.webp",
         time: "1 week ago",
     },
     {
         user: "Nathan Peterson",
+        userImage: "assets/images/avatar-nathan-peterson.webp",
         type: "react",
         post: "5 end-game strategies to increase your win rate",
         time: "2 weeks ago",
     },
     {
         user: "Anna Kim",
+        userImage: "assets/images/avatar-anna-kim.webp",
         type: "left",
         group: "Chess Club",
         time: "2 weeks ago",
@@ -64,19 +72,24 @@ function importData(arr) {
 function createList(obj) {
     const li = document.createElement("li");
     
+    const profileSpan = document.createElement("Span");
+    const profileImg = document.createElement("img");
+    const contentSpan = document.createElement("span");
     const nameSpan = document.createElement("span");
     const dataSpan = document.createElement("span");
     const detailSpan = document.createElement("span");
     const readSpan = document.createElement("span");
-    const messagePart = document.createElement("p");
     const timePart = document.createElement("p");
+    const commentSpan = document.createElement("span");
+    const commentImg = document.createElement("img");
+    const messagePart = document.createElement("p");
 
+    //set html
+    profileImg.src = obj.userImage;
     timePart.innerText = obj.time;
     nameSpan.innerText = obj.user;
     readSpan.innerText = " ●";
     readSpan.classList.add('readDot');
-
-    //set html
     switch (obj.type) {
         case "join":
             dataSpan.innerText = ` has joined your group`;
@@ -103,29 +116,50 @@ function createList(obj) {
         default:
             break;
     }
+    if (obj.replyPicture !== undefined) { commentImg.src = obj.replyPicture; }
 
     //set css
+    li.style.display ="flex";
+    profileSpan.style.marginTop = "5px";
+    profileImg.style.width = "40px";
+    profileImg.style.borderRadius = "20px";    
+    contentSpan.style.flexGrow = "1";
+    contentSpan.style.marginTop = "5px";
+    contentSpan.style.marginLeft = "20px";
     nameSpan.style.fontWeight = "bold";
     dataSpan.style.color = "hsl(219, 12%, 42%)";
     if (obj.group !== undefined) {detailSpan.style.color = "hsl(219, 85%, 26%)"; }
     else {detailSpan.style.color = "hsl(224, 21%, 14%)"; }
     detailSpan.style.fontWeight = "bold";
     readSpan.style.color = "hsl(1, 90%, 64%)";
+    timePart.style.margin = "5px 0";
     timePart.style.color = "hsl(219, 12%, 42%)";
     if (messagePart !== undefined) {
+        messagePart.style.marginBottom = "5px";
         messagePart.style.color = "hsl(219, 12%, 42%)";
         messagePart.style.border = "1px solid hsl(219, 14%, 63%)";
         messagePart.style.borderRadius = "5px";
     }
+    if (obj.replyPicture !== undefined) {
+        commentSpan.style.marginTop = "5px";
+        commentImg.style.width = "40px";
+        commentImg.style.borderRadius = "5px"; 
+    }
 
     //apply and return
-    li.appendChild(nameSpan);
-    li.appendChild(dataSpan);
-    if (detailSpan.innerText !== "") { li.appendChild(detailSpan); }
-    li.appendChild(readSpan);
-    li.appendChild(timePart);
-    if (messagePart.innerText !== "") { li.appendChild(messagePart); }
-
+    li.appendChild(profileSpan);
+    profileSpan.appendChild(profileImg);
+    li.appendChild(contentSpan);
+    contentSpan.appendChild(nameSpan);
+    contentSpan.appendChild(dataSpan);
+    if (detailSpan.innerText !== "") { contentSpan.appendChild(detailSpan); }
+    contentSpan.appendChild(readSpan);
+    contentSpan.appendChild(timePart);
+    if (messagePart.innerText !== "") { contentSpan.appendChild(messagePart); }
+    if (obj.replyPicture !== undefined) {
+        li.appendChild(commentSpan);
+        commentSpan.appendChild(commentImg);
+    }
     return li
 }
 
